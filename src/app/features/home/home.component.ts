e80e5c7edee45b0cae0838c65fa97016eeeb54b7
cynthiafarah <cynthiafarah112@gmail.com>
2025-02-17 16:57:31 +0200

    test

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -11,42 +11,47 @@ import { SharedModule } from '../../shared/shared.module';
 })
 
 export class HomeComponent {
-  // items = [
-  //   { image: '../assets/about-us.jpg', name: 'Cityscape Painting', price: '$44.00' },
-  //   { image: '../assets/about-us.jpg', name: 'Item 2', price: '$25.00' },
-  //   // ... more items
-  // ];
   value = "??"
   testFunction(index: number) {
-    console.log("Button clicked!");
-    let buttonTrending = document.querySelector('.tabs-button-trending')
-    let buttonArrived = document.querySelector('.tabs-button-arrived')
-    if (buttonArrived && buttonTrending) {
-      this.value = "done"
-      if (index == 1) {
-        buttonTrending.setAttribute("class", "active")
-      }
-    } else {
-      this.value = "not done"
+    // console.log("Button clicked!");
+    // let buttonTrending = document.querySelector('.tabs-button-trending')
+    // let buttonArrived = document.querySelector('.tabs-button-arrived')
+    // if (buttonArrived && buttonTrending) {
+    //   this.value = "done"
+    //   if (index == 1) {
+    //     buttonTrending.setAttribute("class", "active")
+    //   }
+    // } else {
+    //   this.value = "not done"
 
-    }
+    // }
   }
   TabAction(index: number) {
     let buttonTrending = document.querySelector('.tabs-button-trending')
     let buttonArrived = document.querySelector('.tabs-button-arrived')
-    this.value = "done"
-    if (index == 1) {
-      this.value = "trending"
-      if (buttonTrending) {
-        buttonTrending.classList.add("active")
-      } else {
-        this.value = "button trending failed"
-      }
-    }
-    if (index == 2)
-      this.value = "arrived"
+    let trendingContainer = document.getElementById('trending')
+    let ArrivedContainer = document.getElementById('arrived')
 
-  }
+    this.value = "done"
+    if (buttonTrending && buttonArrived && ArrivedContainer && trendingContainer) {
+      if (index === 1) {
+          buttonArrived.classList.remove("active")
+          buttonTrending.classList.add("active")
+          ArrivedContainer.style.display='none'
+          trendingContainer.style.display='flex'
+        }
+        else if(index === 2){
+         buttonTrending.classList.remove("active")
+        buttonArrived.classList.add("active")
+        ArrivedContainer.style.display='flex'
+          trendingContainer.style.display='none'
+      } 
+      }  
+    }
+    
+  
+
+   
 
 }
 

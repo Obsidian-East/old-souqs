dbf2caaaffcc80da7b27e4c2de6daefe1e95260e
cynthiafarah <cynthiafarah112@gmail.com>
2025-03-05 00:12:15 +0200

    about-contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { SignupComponent } from './features/signup/signup.component';
 import { LoginComponent } from './features/login/login.component';
+import { AboutUsComponent } from './features/about-us/about-us.component';
+import { ContactComponent } from './features/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'aboutus', component: AboutUsComponent},
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({

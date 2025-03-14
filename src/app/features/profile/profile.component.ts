6eae393a3721130ef8d80366a5e619c00161fee0
REXACKER <dibehjad02@gmail.com>
2025-03-14 12:05:39 +0200

    Connected Signup to profile

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';  // Import Router
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -13,36 +13,31 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-
 export class SignupComponent {
   signupForm = new FormGroup({
     firstname: new FormControl('', Validators.required),
-    lastname: new FormControl('',Validators.required),
+    lastname: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phonenumber: new FormControl('',Validators.required),
-    location: new FormControl('',Validators.required),
+    phonenumber: new FormControl('', Validators.required),
+    location: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   });
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) { }  // Inject Router
 
   handleSubmit() {
     if (this.signupForm.valid) {
-      console.log("Form Data:", this.signupForm.value); // Debugging Line
       this.authService.signup(this.signupForm.value).subscribe(
         (response) => {
           console.log('Signup successful', response);
           alert('Signup successful!');
+          this.router.navigate(['/profile']);  // Redirect to profile component
         },
         (error) => {
           console.error('Signup failed', error);
           alert('Signup failed. Please try again.');
         }
       );
-    } else {
-      console.error("Form is invalid", this.signupForm.errors);
-      alert("Please fill out the form correctly.");
     }
   }
-  
 }

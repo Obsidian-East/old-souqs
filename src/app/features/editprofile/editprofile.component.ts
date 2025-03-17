0558c56d6435b2e7a2eb9f3bda4570d313d6fa94
cynthiafarah <cynthiafarah112@gmail.com>
2025-03-17 22:13:47 +0200

    profile-editprofile

--- src/app/features/editprofile/editprofile.component.ts
+++ src/app/features/editprofile/editprofile.component.ts
@@ -22,40 +22,4 @@ export class EditprofileComponent {
 
 
 
-
-
-
-  profileImage: string  = this.userData.profilepic;
-  errorMessage: string = '';
-
-  onFileSelected(event: Event) {
-    const input = event.target as HTMLInputElement;
-    
-    if (input.files && input.files[0]) {
-      const file: File = input.files[0];
-
-      // Validate file type (only images)
-      if (!file.type.startsWith('image/')) {
-        this.errorMessage = 'Please select a valid image file.';
-        return;
-      }
-
-      // Validate file size (max 2MB)
-      if (file.size > 2 * 1024 * 1024) {
-        this.errorMessage = 'File size should be less than 2MB.';
-        return;
-      }
-
-      // File is valid, read and preview
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.profileImage = e.target?.result as string;
-      };
-      reader.readAsDataURL(file);
-
-      // Clear error message
-      this.errorMessage = '';
-    }
-  }
-
 }

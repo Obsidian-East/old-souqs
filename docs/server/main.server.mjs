47b2be67cca99f69545cfd9414a82216f6dd82b3
REXACKER <dibehjad02@gmail.com>
2025-04-24 09:29:40 +0300

    Add production build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QFK274JC.mjs";import"./chunk-3YZTSWLG.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-XNKDF4KY.mjs";import"./chunk-EBZXJOP4.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
